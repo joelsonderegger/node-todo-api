84e7d733bd454f7b157944820a10bece54ad28d6
joelsonderegger <joel.sonderegger@googlemail.com>
2018-03-28 07:44:46 +0200

    beautify delete and update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,9 +1,12 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-    if(err) {
+    if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connted to MongoDB server');
